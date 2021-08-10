9c12a2d1c5d40c98df2483ada30dc5b78e4b563c
prabhuprasath <prabhuprasath02@gmail.com>
2021-08-10 20:53:38 +0530

    simple log function is created

---
+++ index.js
@@ -0,0 +1,12 @@
+let myName = "prabhu prasath";
+
+function logmeessage() {
+
+    if(myName==="prabhu prasath"){
+        console.log("hello prabhu prasath");
+    
+    }else{
+        console.log("hello learner!");
+    }
+    
+}logmeessage();
